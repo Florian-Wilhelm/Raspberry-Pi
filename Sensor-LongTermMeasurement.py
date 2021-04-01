7789d7e58a9317d449742d4f696d568f5e1817c7
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2021-04-01 07:24:52 +0200

    Update Sensor-LongTermMeasurement.py
    Included BMP280 measurement (temperature).

--- Sensor-LongTermMeasurement.py
+++ Sensor-LongTermMeasurement.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+import Adafruit_BMP.BMP280 as BMP280
 import RPi.GPIO as GPIO
 import time
 import datetime
@@ -6,21 +7,22 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
-# Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
 GPIO.setmode(GPIO.BCM)
 moistureLevel1_percent = 0.0
 moistureLevel2_percent = 0.0
-sleepZeit = 3600 
-zeitStunden = 24 # Gesamtlaufzeit
-gesamtZeit = zeitStunden*sleepZeit # 1h = 3600s
+sleepZeitSekunden = 3600 # 1h = 3600s
+LaufzeitWert = 24 
+gesamtZeitSekunden = LaufzeitWert*sleepZeitSekunden 
 i=0
 n=0
+tempSensor = BMP280.BMP280()
 workbook = xlsxwriter.Workbook("LTM.xlsx")
 worksheet = workbook.add_worksheet()
-while i<gesamtZeit:   
+while i<gesamtZeitSekunden:
+       # Temperaturmessung
+       temperature = tempSensor.read_temperature()   
        # Sensor 1
        GPIO.setup(27, GPIO.OUT)
        GPIO.output(27, GPIO.HIGH)
@@ -38,10 +40,11 @@ while i<gesamtZeit:
        moistureLevel2_percent = (chan1.voltage/1.7)*100
        time.sleep(0.1)
        GPIO.output(12, GPIO.LOW)
-       # Writing
+       # Writing       
        worksheet.write(n,0, moistureLevel1_percent)
        worksheet.write(n,1, moistureLevel2_percent)
+       worksheet.write(n,2, temperature)
        n = n+1
-       i=i+sleepZeit
-       time.sleep(sleepZeit)
+       i=i+sleepZeitSekunden
+       time.sleep(sleepZeitSekunden)
 workbook.close()
