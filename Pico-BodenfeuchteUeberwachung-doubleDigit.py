6f93556caa05037a0fae31d631711a3fb5c3c9f0
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2021-09-15 16:53:40 +0200

    Update Pico-BodenfeuchteUeberwachung-doubleDigit.py

--- Pico-BodenfeuchteUeberwachung-doubleDigit.py
+++ Pico-BodenfeuchteUeberwachung-doubleDigit.py
@@ -20,7 +20,7 @@ while True:
     if ExpandedSensorValueS > 65535:
       ExpandedSensorValueS = 65535    
     # Driving 7-Segment-Displays, ranges equidistant
-    # there exist more elegant algorithms to set the pins high/low (manipulating the registers), what you see below is quite lengthy
+    # there exist more elegant algorithms to set the pins high/low (using the Single-cycle IO block (SIO)), what you see below is quite lengthy
     # 0-9
     if ((ExpandedSensorValueS>=0) and (ExpandedSensorValueS<=655)):      
       A0.low()      
