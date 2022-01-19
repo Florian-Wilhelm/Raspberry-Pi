a13ca5f3f289e3b7f2a8680c272daa80ed87db02
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-01-19 06:27:38 +0100

    Update ledTest.py

--- Project-4/ledTest.py
+++ Project-4/ledTest.py
@@ -1,11 +1,11 @@
-# Test-Script zur Ansteuerung zweier LEDs via PWM
+# Test-Script Hell/Dunkel Steuerung zweier LEDs via PWM
 from machine import Pin, PWM
 import utime
 pwm0 = PWM(Pin(0)) # GP0
 pwm0.freq(1000) 
 pwm1 = PWM(Pin(1)) # GP1
 pwm1.freq(1000) 
-# LED1 wird heller, LED2 wird dunkler
+# LED1 wird heller, LED2 wird dunkler (und vice versa)
 def ramp(dutyCycleBin, puls):
    if dutyCycleBin == 65536: # 65536 ausserhalb des erlaubten Bereiches
       dutyCycleBin = 65535
@@ -17,7 +17,7 @@ def ramp(dutyCycleBin, puls):
 groesstwert = pow(2, 16) # 2^16 = 65536
 # Startwert (bitte nicht ändern)
 dutyCycleBin = 0
-# Zeit die ein PWM-Wert erhalten bleibt
+# Zeit (in Sekunden) die ein PWM-Wert erhalten bleibt
 puls = 0.003
 # Durchlauf
 inkrement = pow(2, 8) # muss 2er Potenz sein
