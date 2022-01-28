5c3fb0f744d721dc86b460c0c74b8a59b3da4b9a
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-01-28 08:52:55 +0100

    Update motorTaster.py

--- Project-4/motorTaster.py
+++ Project-4/motorTaster.py
@@ -1,7 +1,7 @@
 # Programm reagiert auf eine äußere Eingabe (Taster) und treibt einen DC-Motor an;
-# Code könnte auch sehr simpel in C implementiert werden.
+# Algorithmus kann auch sehr simpel in C/C++ implementiert werden.
 from machine import Pin, PWM
-Taster = Pin(5, Pin.IN) # Pin 5 an Pull-Down Widerstand, Tasterbetätigung führt zu logisch High
+Taster = Pin(5, Pin.IN) # Pin 5 an Pull-Down Widerstand (kann 10k sein), Tasterbetätigung führt zu logisch High
 pwm = PWM(Pin(1)) 
 pwm.freq(1000) # PWM-Frequenz
 while True:
