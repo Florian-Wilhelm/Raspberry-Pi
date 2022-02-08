d596bacd310a678de42758e31d205234fc9166cc
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-02-08 07:16:02 +0100

    Update GPIO5__init.py

--- Project-1/GPIO5__init.py
+++ Project-1/GPIO5__init.py
@@ -1,3 +1,5 @@
+# Dieses Skript dient dazu, GPIO5 sicher auf High zu setzen nachdem das erste Python-Skript ausgeführt wurde (werden sequentiell über ein Shell-Skript aufgerufen);
+# bei fehlerhafter Ausführung oder Abbruch des ersten Skripts könnte andernfalls die Pumpe dauerhaft laufen.
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False) 
