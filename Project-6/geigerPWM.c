b224bc39a7f60514f73748ebf2959ddd54dc57f0
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-04-21 07:20:31 +0200

    Update geigerPWM.c

--- Project-6/geigerPWM.c
+++ Project-6/geigerPWM.c
@@ -11,13 +11,13 @@
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d)
 {
     uint32_t clock = 125000000;
-    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0); // "(clock % (f * 4096) != 0)" - dieser Ausdruck ist entweder 1 (true) oder 0 (false)
     if (divider16 / 16 == 0)
       divider16 = 16;
     uint32_t wrap = clock * 16 / divider16 / f - 1;
     pwm_set_clkdiv_int_frac(slice_num, divider16/16, divider16 & 0xF);
-    pwm_set_wrap(slice_num, wrap);
-    pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+    pwm_set_wrap(slice_num, wrap); // setzt die PWM Frequenz 
+    pwm_set_chan_level(slice_num, chan, wrap * d / 100); // setzt den Tastgrad
     return wrap;
 }
 
