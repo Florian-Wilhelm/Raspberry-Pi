94052f2402d1e8d03f30e56b51d45849c4e80c14
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-05-06 06:11:17 +0200

    Update geigerPWM.c

--- Project-6/geigerPWM.c
+++ Project-6/geigerPWM.c
@@ -11,15 +11,15 @@
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d)
 {
     uint32_t clock = 125000000; // default clock 125 MHz
-    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0); // "(clock % (f * 4096) != 0)" - dieser Ausdruck ist entweder 1 (true) oder 0 (false)
+    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0); // "(clock % (f * 4096) != 0)" - either 1 (true) or 0 (false)
     if (divider16 / 16 == 0)
       divider16 = 16;
     uint32_t wrap = clock * 16 / divider16 / f - 1;
     
     // pwm_set_phase_correct (slice_num, false);
     pwm_set_clkdiv_int_frac(slice_num, divider16/16, divider16 & 0xF);
-    pwm_set_wrap(slice_num, wrap); // setzt die PWM Frequenz 
-    pwm_set_chan_level(slice_num, chan, wrap * d / 100); // setzt den Tastgrad
+    pwm_set_wrap(slice_num, wrap); // setting the PWM frequency 
+    pwm_set_chan_level(slice_num, chan, wrap * d / 100); // setting the duty cycle
     return wrap;
 }
 
@@ -39,7 +39,7 @@ int main() {
     pwm_set_freq_duty(slice_num, chan, usedFrequency, 60);
     pwm_set_enabled(slice_num, true);
     
-    // once set, it runs forever; no "while(1)" necessary in this case
+    // once set, it runs forever; no "while(1)" necessary in this program
     
     return 0;    
 }
