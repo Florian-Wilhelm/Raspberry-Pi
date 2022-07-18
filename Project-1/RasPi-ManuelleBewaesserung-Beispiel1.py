17049b720ae417257aca9381b53f698ce0e32109
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-07-18 09:17:39 +0200

    Update RasPi-ManuelleBewaesserung-Beispiel1.py

--- Project-1/RasPi-ManuelleBewaesserung-Beispiel1.py
+++ Project-1/RasPi-ManuelleBewaesserung-Beispiel1.py
@@ -5,9 +5,7 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
-# Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
 GPIO.setmode(GPIO.BCM)
 # Variables
