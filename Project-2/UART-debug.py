f7f64aab208166022db7667f7d553d4ad33a3345
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-08-02 06:48:48 +0200

    Update UART-debug.py

--- Project-2/UART-debug.py
+++ Project-2/UART-debug.py
@@ -7,13 +7,13 @@ from machine import Pin
 #print sys info
 #print(os.uname())
 
-# bei jedem Sendebeginn auf UART TX wird das Signal auf 0 gezogen -> wird ausgenutzt zur Triggerung der Ausgabe (elektrische Verbindung notwendig, GP15 willkürlich)
+# bei jedem Sendebeginn auf UART TX wird das Signal auf 0 gezogen -> wird ausgenutzt zur Triggerung der Ausgabe (extra elektrische Verbindung notwendig, GP15 willkürlich)
 trigger = Pin(15, Pin.IN)
 
 # visuelle Indikation dass das Programm läuft 
 led_onboard = machine.Pin(25, machine.Pin.OUT)
 led_onboard.value(0)
-utime.sleep(0.5)
+utime.sleep(1)
 led_onboard.value(1)
 
 uart0 = machine.UART(0)  # UART0: GP0 und GP1 (hier nur RX, also GP1 notwendig)
@@ -23,8 +23,8 @@ utime.sleep(0.1)
 
 while True:
   if trigger.value() == 0:     
-    while uart0.any():  # zählt herunter bis keine Zeichen mehr zur Ausgabe vorhanden sind    
-      print(uart0.read(1))  # # lies 1 character
+    while uart0.any():  # zählt herunter bis keine Zeichen (character) mehr zur Ausgabe vorhanden sind    
+      print(uart0.read(1))  # lies ein Zeichen
     
     print()    
     
