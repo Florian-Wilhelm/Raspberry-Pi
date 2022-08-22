d9d2de853310ed41c1b8284569984c055d9c01dc
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-08-22 08:06:54 +0200

    Update Pico-Demonstration-SIO.c

--- Project-2/Pico-Demonstration-SIO.c
+++ Project-2/Pico-Demonstration-SIO.c
@@ -1,7 +1,7 @@
 /**
  * Created 06/07/2022 by Florian Wilhelm Dirnberger
  *
- * Demonstration how to write several Raspberry Pi Pico GPIO ports at once by using the Single-cycle IO block (SIO)
+ * Purely a demonstration code how to write several Raspberry Pi Pico GPIO ports at once by using the Single-cycle IO block (SIO)
  * See figure 2 and chapter 2.3.1 in the RP2040 datasheet
  * "The SIO is connected to the single-cycle IOPORT bus of each processor, and provides GPIO access, two-way communications, and other core-local peripherals".
  *
