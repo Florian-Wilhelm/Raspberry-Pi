b4d97aca3a09304fd5160af43b5b6b93095c4811
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-10-30 10:44:56 +0100

    Update geigerCounter.c

--- Project-6/geigerCounter.c
+++ Project-6/geigerCounter.c
@@ -1,12 +1,17 @@
 /**
- * Created 01/10/2022 by Florian Wilhelm Dirnberger
+ * Created 30/10/2022 by Florian Wilhelm Dirnberger
  *
- * V1.0 Geiger counter program running on a Raspberry Pi Pico
- *   
- * Outputs a PWM signal for HV-generation, able to trigger one or several simple output devices (LEDs, buzzer etc.) in case of an interrupt (counting event)
- * In addition, an LCD will show some activity (counts, elapsed time)
+ * Geiger counter program for running on a Raspberry Pi Pico with attached prototype PCB
  * 
- * Pull-down/up resistors are disabled in this variant (push-pull mode) for the IRQ GP; depends on the connected HW revision whether that makes sense
+ * this SW-version:
+ * V1.1
+ * to be used with HW revision:
+ * HW2.0 and below
+ *   
+ * HV-generation by virtue of a PWM (for the boost-converter); triggers simple output devices (LEDs, buzzer etc.) in case of an interrupt (counting event)
+ * In addition, an LCD will show counting events and the elapsed time (in lieu of CPM)
+ * 
+ * note: Pull-down/up resistors are disabled in this variant (push-pull mode) for the IRQ GP
  * 
  */
 
@@ -18,6 +23,10 @@
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
 #include "pico/time.h"
+
+#define MASK (1<<8) | (1<<9) | (1<<25) // flexible output GPIOs (only for prototype stage); available: GP0-GP15 and user LED Pin 25
+
+// instructions for the LCD (generic)
 
 // commands
 const int LCD_CLEARDISPLAY = 0x01;
@@ -123,17 +132,17 @@ void lcd_init() {
     lcd_clear();
 }
 
-const uint userIRQgpio = 17;
+// now the actual Geiger counter program
 
+// fixed GPIOs of prototype Rev 2.0
+const uint userIRQgpio = 17;
 const uint usedPWMgpio = 16;
+
+// empirical PWM value
 const uint usedPWMfreq = 1250;
 
-uint zaehlung = 0;
-
-char snum[5];
-
-// Output GP
-const uint32_t mask=(1<<8) | (1<<9) | (1<<25); // Pin 25: the green user LED; other GPs can be used for additional LEDs or a buzzer module
+// flexible output GPIOs of prototype Rev 2.0
+const uint32_t mask=MASK; 
 
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan,uint32_t f, int d) {
     uint32_t clock = 125000000;
@@ -147,15 +156,19 @@ uint32_t pwm_set_freq_duty(uint slice_num, uint chan,uint32_t f, int d) {
     return wrap;
 }
 
+uint countingEvent = 0;
+
 void geiger_irq_handler(uint gpio, uint32_t events) {
     if (events == GPIO_IRQ_EDGE_FALL) {
         gpio_set_mask(mask); 
-        zaehlung += 1; }     
+        countingEvent += 1; }     
     else if (events == GPIO_IRQ_EDGE_RISE) 
         gpio_clr_mask(mask);                   
     /*else {
         }*/
 }
+
+char snum[5];
 
 int main() {   
          
@@ -176,17 +189,25 @@ int main() {
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 
     lcd_init();
+    
     lcd_clear();  
+    lcd_set_cursor(0,0);  
+    lcd_string("SW");
+    lcd_set_cursor(0,8);  
+    lcd_string("PWM (Hz)");
+    lcd_set_cursor(1,0);
+    lcd_string("V1.1"); 
+    itoa(usedPWMfreq, snum, 10);
+    lcd_set_cursor(1,8);
+    lcd_string(snum);               
+    
+    sleep_ms(2000);
+    
+    lcd_clear();
     lcd_set_cursor(0,0);  
     lcd_string("Pulse");
     lcd_set_cursor(0,8);  
-    lcd_string("Zeit(s)");
-    lcd_set_cursor(1,0);
-    lcd_string("-"); 
-    lcd_set_cursor(1,8);
-    lcd_string("-");               
-    
-    sleep_ms(2000);    
+    lcd_string("Zeit(s)");    
     
     pwm_set_freq_duty(slice_num, chan, usedPWMfreq, 60);
     pwm_set_enabled(slice_num, true);
@@ -203,7 +224,7 @@ int main() {
           
        uint AbsolutZeit = (to_us_since_boot(get_absolute_time())/1000000);            
                       
-       itoa(zaehlung, snum, 10); 
+       itoa(countingEvent, snum, 10); 
        lcd_set_cursor(1,0);
        lcd_string(snum); 
             
