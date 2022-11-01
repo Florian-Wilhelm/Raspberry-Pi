8e37ca9d980f5e1895241eb1e44a3995410fefd3
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-11-01 07:34:38 +0100

    Update pio.c

--- Project-8/pio.c
+++ Project-8/pio.c
@@ -1,7 +1,7 @@
 /**
- * Created 20/10/2022 by Florian Wilhelm Dirnberger
+ * Created 31/10/2022 by Florian Wilhelm Dirnberger
  *
- * Code is entirely copied from Harry Fairhead's book, page 241.
+ * Code is copied from Harry Fairhead's book, page 244 (some modifications).
  * 
  */
 
@@ -12,17 +12,18 @@
 
 int main()
 {
-    uint offset = pio_add_program(pio0, &squarewave_program);
+    uint offset = pio_add_program(pio1, &squarewave_program);
     
-    uint sm = pio_claim_unused_sm(pio0, true); // the SDK can choose the state machine (there are four in each PIO)
-    pio_sm_config c = squarewave_program_get_default_config(offset); // creating the struct
+    uint sm = pio_claim_unused_sm(pio1, true); // the SDK chooses the s(tate) m(achine) (4 sm available)
+    pio_sm_config c = squarewave_program_get_default_config(offset); // setting up the struct
     
-    sm_config_set_clkdiv_int_frac(&c,255,0);
+    sm_config_set_set_pins(&c,25,1);
+    pio_gpio_init(pio1, 25);
     
-    sm_config_set_set_pins(&c, 25, 1); // Set the 'set' pins (here: 25, one pin to set) in a state machine configuration
-    pio_gpio_init(pio0, 25);
-    
-    pio_sm_init(pio0, sm, offset, &c);
-    pio_sm_set_enabled(pio0, sm, true);
+    sm_config_set_clkdiv_int_frac(&c,255,0); 
+    pio_sm_init(pio1, sm, offset, &c);    
+    pio_sm_set_enabled(pio1, sm, true);
+        
+    pio_sm_put_blocking(pio1, sm, 0x0001FFFF); // stores a 32-bit value in the TX FIFO   
     return 0;
 }
