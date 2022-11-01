befe4639cda8226209136f0c3acdef8fdb0241d5
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-11-01 09:28:13 +0100

    Update IoT_Pico--HW390-GP26--103AT2-GP27.c

--- Project-2/IoT_Pico--HW390-GP26--103AT2-GP27.c
+++ Project-2/IoT_Pico--HW390-GP26--103AT2-GP27.c
@@ -8,7 +8,7 @@
  * HW-390 capacitive soil moisture sensor - ADC0 (GP26)
  * NTC temperature sensor 103AT2 (as part of a voltage divider) - ADC1 (GP27)
  * 
- * note 1: WiFi-credentials have to be modified (name, password)
+ * note 1: WiFi-credentials have to be modified (name, password); without that it would make no sense to compile this source code
  * note 2: this is NOT a Pico W project; used is a normal Pico and an Espressif ESP8266 µC
  *
  */
