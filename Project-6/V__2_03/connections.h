932e2e7ce0afd584bb035c87cf764747f5742a98
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2022-12-17 07:38:43 +0100

    Update connections.h

--- Project-6/V__2_03/connections.h
+++ Project-6/V__2_03/connections.h
@@ -1,7 +1,7 @@
 #ifndef __CONNECTIONS_H__
 #define __CONNECTIONS_H__
 
-// Flexible output GPIOs (only for prototype stage); GPs not soldered to PCB, therefore available: GP0-GP15; user LED Pin 25
+// Flexible output GPIOs (only for prototype stage); user LED Pin 25
 #define DEVICEMASK (1<<8) | (1<<9) | (1<<25) 
 
 // PWM frequency
