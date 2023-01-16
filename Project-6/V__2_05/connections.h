94e4c83514b797bc8927eaee81a5783cc7ebcbe2
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2023-01-16 07:48:54 +0100

    Add files via upload

---
+++ Project-6/V__2_05/connections.h
@@ -0,0 +1,24 @@
+#ifndef __CONNECTIONS_H__
+#define __CONNECTIONS_H__
+
+// Flexible output GPIOs (only for prototype stage); GPs not soldered to PCB, therefore available: GP0-GP15; user LED Pin 25
+#define DEVICEMASK (1<<8) | (1<<9) | (1<<25) 
+
+// PWM frequency
+#define PWM_FQ 1000
+
+// PWM out GP
+#define PWM_GP 3
+
+// IRQ GP
+#define IRQ_GP 2
+
+// X GP
+#define X_GP 22
+
+// I2C Pins for the LCD
+#define DISPLAY_SDA_PIN 4
+#define DISPLAY_SCL_PIN 5
+#define I2C_ID i2c0 // this is not an independent parameter, change it according to chosen SDA and SCL pins (see Pico datasheet)
+
+#endif
