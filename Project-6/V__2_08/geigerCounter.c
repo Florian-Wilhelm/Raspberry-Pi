df3de713e9a8453aa4d0263c343991165282e8d4
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2023-04-30 07:16:53 +0200

    Update geigerCounter.c

--- Project-6/V__2_08/geigerCounter.c
+++ Project-6/V__2_08/geigerCounter.c
@@ -140,7 +140,7 @@ void lcd_init() {
 
 // now the actual Geiger counter program
 
-// this is not necessarily an optimized calculation for generating one particular frequency and duty cycle, but it works sufficiently for the given purpose
+// this can be done better
 void pwm_set_freq_duty(uint32_t f_clk_sys, uint slice_num, uint chan, uint32_t f, int d) {
     uint32_t clock = f_clk_sys;
     uint32_t divider16 = clock / (f * 4096) + (clock % (f * 4096) != 0);
@@ -170,7 +170,7 @@ int main() {
     
     //stdio_init_all(); 
     
-    // changing the system clock frequency (handle with care), will change the PWM freq    
+    // changing the system clock   
     
     /*clock_configure(clk_sys,
                     CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
