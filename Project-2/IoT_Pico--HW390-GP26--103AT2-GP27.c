97129d06564f728a9bc3a5acb607f44b9cc7974f
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2023-08-05 07:58:37 +0200

    Update IoT_Pico--HW390-GP26--103AT2-GP27.c

--- Project-2/IoT_Pico--HW390-GP26--103AT2-GP27.c
+++ Project-2/IoT_Pico--HW390-GP26--103AT2-GP27.c
@@ -7,6 +7,7 @@
  * 
  * note 1: WiFi-credentials have to be modified (name and password, command "connectWiFi"); without that there is no point compiling this source code
  * note 2: as you may have guessed this is NOT a Pico W project; this assembly uses the classic Pico and an Espressif ESP8266 MCU
+ *        Wiring ESP8266 - Pico: https://hackaday.io/project/178522-soil-moisture-measurement-device/log/207966-iot-capabilities
  *
  */
 
