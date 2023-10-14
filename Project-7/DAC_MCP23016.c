d9aa92df16a6d674bf270cbe688b02e5892cec11
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2023-10-14 07:32:35 +0200

    Update DAC_MCP23016.c

--- Project-7/DAC_MCP23016.c
+++ Project-7/DAC_MCP23016.c
@@ -2,7 +2,7 @@
  * Created 03/01/2023 by Florian Wilhelm Dirnberger
  *
  * 6bit DAC hardwired to MCP23016 digital input/output Pins GP0.x (IC is a I/O expansion for I2C bus) 
- * MCP23016 in turn is connected to one of the Pico I2C controllers (i.e. I2C master) for receiving bus data
+ * MCP23016 in turn is connected to one of the two RP2040 I2C controllers (i.e. I2C master) for receiving bus data
  * 
  * Realized with this program is a simple saw tooth generator
  *  
@@ -24,10 +24,10 @@ int main()
   
   sleep_ms(100);
 
-  i2c_init(i2c1, 50*1000);
+  i2c_init(i2c1, 50*1000); // second parameter: I2C frequency
 
-  gpio_set_function(2, GPIO_FUNC_I2C);
-  gpio_set_function(3, GPIO_FUNC_I2C);
+  gpio_set_function(2, GPIO_FUNC_I2C); // SDA
+  gpio_set_function(3, GPIO_FUNC_I2C); // SCL
 
   // using two external 3.9kOhm resistors, so the following two lines are obsolete
   /* gpio_pull_up(2);
@@ -48,7 +48,7 @@ int main()
   
   while(true){
          
-   for (sawtooth = 0x3F; sawtooth>=0x00; sawtooth--) // only a 6bit DAC, so we count down from 00111111 which is 0x3F (inverted input for the DAC)
+   for (sawtooth = 0x3F; sawtooth>=0x00; sawtooth--) // a 6bit DAC, so we count down from 00111111 which is 0x3F, or 63d (inverted input for the DAC)
     {
   
     buf[0] = 0x00; // data port register
@@ -56,7 +56,7 @@ int main()
      
     i2c_write_blocking(i2c1, 0x20, buf, 2, false);  
   
-    //sleep_us(1000); // will manipulate the sawtooth frequency, along with the baud rate
+    //sleep_us(1000); // a value here would manipulate the sawtooth frequency, along with the determined I2C frequency
     
     }  
   }         
