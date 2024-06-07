54dbf63bfda78de8c6a709f7707776904cd182b2
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2024-06-07 17:48:49 +0200

    Add files via upload

---
+++ Project-2/SD-Card.py
@@ -0,0 +1,24 @@
+import machine
+import sdcard
+import uos
+
+cs = machine.Pin(17, machine.Pin.OUT)
+
+spi = machine.SPI(0,
+                  baudrate=1000000,
+                  polarity=0,
+                  phase=0, 
+                  bits=8,
+                  firstbit=machine.SPI.MSB,
+                  sck=machine.Pin(18),
+                  mosi=machine.Pin(19),
+                  miso=machine.Pin(16))
+
+sd = sdcard.SDCard(spi, cs)
+
+vfs = uos.VfsFat(sd)
+uos.mount(vfs, "/sd")
+
+with open("/sd/test01.txt", "w") as file:
+   file.write("RH test\r\n")
+                  
