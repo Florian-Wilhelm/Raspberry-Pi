328d4e6aa93f3feb749b26ec606599b21e2d6111
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2025-07-28 16:27:59 +0200

    Update CPMinfo.php

--- Project-6/CPMinfo.php
+++ Project-6/CPMinfo.php
@@ -1,7 +1,8 @@
 <?php
-// phpinfo(); // liefert Informationen zur PHP-Konfiguration
-// serieller Datentransfer Pico --> Raspberry Pi, Ausgabe der Daten im Webbrowser mit diesem php-Skript (s.a. "Getting started with Raspberry Pi Pico", Kap. 4.5)
+// serieller Datentransfer Pico --> Raspberry Pi, Ausgabe der übertragenen Daten im Webbrowser mit diesem php-Skript (s.a. "Getting started with Raspberry Pi Pico", Kap. 4.5)
 // Raspberry Pi Shell: "sudo minicom -b 115200 -o -D /dev/serial0 -C captureFileCPM.txt" zum Erzeugen der Textdatei
+//
+// phpinfo(); // liefert Informationen zur PHP-Konfiguration (brauchen wir hier nicht)
 echo "Counts Per Minute (CPM), transmitted by Geiger Counter HW revision 3.2";
 echo '<br />';
 $inhalt = file_get_contents('/home/pi/captureFileCPM.txt');
