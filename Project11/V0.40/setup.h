f9d00e4f5a39cc4454787c7fb865c8ca02dce356
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2025-09-24 07:51:51 +0200

    Add files via upload

---
+++ Project11/V0.40/setup.h
@@ -0,0 +1,79 @@
+int setup(uint32_t country, const char *ssid, const char *pass,
+          uint32_t auth, const char *hostname, ip_addr_t *ip,
+          ip_addr_t *mask, ip_addr_t *gw)
+{
+
+    if (cyw43_arch_init_with_country(country))
+    {
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+    if (hostname != NULL)
+    {
+        netif_set_hostname(netif_default, hostname);
+    }
+    if (cyw43_arch_wifi_connect_async(ssid, pass, auth))
+    {
+        return 2;
+    }
+    int flashrate = 1000;
+    int status = CYW43_LINK_UP + 1;
+    while (status >= 0 && status != CYW43_LINK_UP)
+    {
+        int new_status = cyw43_tcpip_link_status(&cyw43_state,
+                                                 CYW43_ITF_STA);
+        if (new_status != status)
+        {
+            status = new_status;
+            flashrate = flashrate / (status + 1);
+            printf("connect status: %d %d\n", status, flashrate);
+        }
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(flashrate);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(flashrate);
+    }
+    if (status < 0)
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    }
+    else
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        if (ip != NULL)
+        {
+            netif_set_ipaddr(netif_default, ip);
+        }
+        if (mask != NULL)
+        {
+            netif_set_netmask(netif_default, mask);
+        }
+        if (gw != NULL)
+        {
+            netif_set_gw(netif_default, gw);
+        }
+
+        printf("IP: %s\n",
+               ip4addr_ntoa(netif_ip_addr4(netif_default)));
+        printf("Mask: %s\n",
+               ip4addr_ntoa(netif_ip_netmask4(netif_default)));
+        printf("Gateway: %s\n",
+               ip4addr_ntoa(netif_ip_gw4(netif_default)));
+        printf("Host Name: %s\n",
+               netif_get_hostname(netif_default));
+    }
+    return status;
+}
+
+int connect()
+{
+   char ssid[] = "WLAN-ABC"; // obscured for privacy reasons
+   char pass[] = "12345"; // obscured for privacy reasons
+   uint32_t country = CYW43_COUNTRY_GERMANY;
+   uint32_t auth = CYW43_AUTH_WPA2_MIXED_PSK;
+   return setup(country, ssid, pass, auth, "Pico W", NULL, NULL, NULL);
+}
+
+// lwipopts.h source:
+// /home/pi/pico/pico-sdk/lib/lwip/src/include/lwip
