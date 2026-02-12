c9d838a9e2bbb95b23013afe603ef8169289d087
Florian Wilhelm Dirnberger <77980708+Florian-Wilhelm@users.noreply.github.com>
2026-02-12 15:02:59 +0100

    Add files via upload

---
+++ Project11/V0.29/connections.h
@@ -0,0 +1,20 @@
+#ifndef __CONNECTIONS_H__
+#define __CONNECTIONS_H__
+
+// I2C Pins
+#define SDA_PIN 4
+#define SCL_PIN 5
+
+#define I2C_ID i2c0 
+
+// UART Pins
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+#define UART_ID uart0
+
+// dip switch Pins
+#define DIP_SWITCH_PIN2 14
+#define DIP_SWITCH_PIN3 15
+
+#endif
